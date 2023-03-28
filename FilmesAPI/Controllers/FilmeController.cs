c795e419a0a78ae9d66c66dce092e3d309ce9474
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-03-28 20:54:06 -0300

    Criação do método GET filmes por Id

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,17 +8,27 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public string AdicionaFilme([FromBody] Filme filme)
         {
+            filme.Id = Id++;
             filmes.Add(filme);
+            return $"'{filme.Titulo}' adicionado com sucesso!"; 
         }
 
         [HttpGet]
         public IEnumerable<Filme> BuscarFilmes()
         {
             return filmes;
+        }
+
+        [HttpGet("{idFilme}")]
+        public Filme? BuscarFilmePorId(int idFilme)
+        {
+            var output = filmes.FirstOrDefault(f => f.Id.Equals(idFilme));
+            return output;
         }
     }
 }
