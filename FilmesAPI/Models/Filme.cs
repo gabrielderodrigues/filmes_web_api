25b22860b6d7fcce385386fb916e17f777b0e5d2
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-03-28 20:19:27 -0300

    Validando parâmetros recebidos.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O título do filme é obrigatório.")]
+        public string? Titulo { get; set; }
+
+        [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
+        public string? Genero { get; set; }
+
+        [Required(ErrorMessage = "A duração do filme é obrigatório.")]
+        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 a 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
