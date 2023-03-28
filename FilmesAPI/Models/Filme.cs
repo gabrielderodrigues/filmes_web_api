14ce94e2411769a5d3b2f2ecda8d18145b9459f5
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-03-28 20:22:40 -0300

    Adicionando propriedade Diretor el Filme.

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -5,10 +5,13 @@ namespace FilmesAPI.Models
     public class Filme
     {
         [Required(ErrorMessage = "O título do filme é obrigatório.")]
-        public string? Titulo { get; set; }
+        public string Titulo { get; set; }
 
         [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
-        public string? Genero { get; set; }
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O filme precisa ter um diretor.")]
+        public string Diretor { get; set; }
 
         [Required(ErrorMessage = "A duração do filme é obrigatório.")]
         [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 a 600 minutos.")]
