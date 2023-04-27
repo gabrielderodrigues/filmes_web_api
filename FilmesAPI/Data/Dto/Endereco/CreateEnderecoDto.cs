254ff6cad0c51cd7b17c17a1fddd88898a16d358
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-04-26 21:09:01 -0300

    Criação de Entidade, DTOs e Controller de Endereco.

---
+++ FilmesAPI/Data/Dto/Endereco/CreateEnderecoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmesAPI.Data.Dto.Endereco
+{
+    public class CreateEnderecoDto
+    {
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+    }
+}
